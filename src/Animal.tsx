035106544a9943e5acc6bc06096d180f207bbc6b
Igor Knop <igor.knop@ufjf.br>
2025-10-07 22:03:32 -0300

    :sparkles: adiciona props

--- src/Animal.tsx
+++ src/Animal.tsx
@@ -1,7 +1,20 @@
 import "./Animal.css"
 
-export default function Animal() {
+type AnimalProps = {
+    icone?: string;
+    nome?: string;
+    peso?: number;
+    emExtincao?: boolean;
+}
+
+
+export default function Animal({ icone = "?", nome = "Desconhecido", peso = 0.1, emExtincao = false }: AnimalProps) {
     return (
-        <div className="animal">Animal</div>
+        <div className="animal">
+            <div>{icone}</div>
+            <div>{nome}</div>
+            <div>{peso.toLocaleString()}kg</div>
+            <div>{emExtincao}</div>
+        </div>
     );
 }
