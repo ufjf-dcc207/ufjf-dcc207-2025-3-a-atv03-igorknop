f443d855bf44ed85ac5b862ff7f77af72599c22e
Igor Knop <igor.knop@ufjf.br>
2025-10-09 21:36:36 -0300

    :art: refatora badge

--- src/Animal.tsx
+++ src/Animal.tsx
@@ -9,14 +9,18 @@ type AnimalProps = {
 
 
 export default function Animal({ icone = "?", nome = "Desconhecido", peso = 0.1, emExtincao = false }: AnimalProps) {
+    let badge;
+    if (emExtincao) {
+        badge = <div className="extincao">Ameaçado</div>;
+    } else {
+        badge = <div className="seguro">Seguro</div>;
+    }
     return (
         <div className="animal">
             <div className="icone">{icone}</div>
             <div className="nome">{nome}</div>
             <div className="peso">{peso.toLocaleString()}kg</div>
-            {emExtincao ? <div className="extincao">Ameaçado</div>
-                : <div className="seguro">Seguro</div>
-            }
+            {badge}
         </div>
     );
 }
