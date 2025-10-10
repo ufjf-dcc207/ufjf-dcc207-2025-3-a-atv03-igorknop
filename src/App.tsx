149ac0a557c79dca238be70d6cc59437629b999d
Igor Knop <igor.knop@ufjf.br>
2025-10-09 22:21:55 -0300

    :art: adiciona children em exibição

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,19 @@ function App() {
   return (
     <div>
       <Exibicao inicio={new Date("2025-10-11T08:00:00-03:00")}
-      fim={new Date("2025-10-11T12:00-03:00")}
-      cercado='ICE'></Exibicao>
-      <Animal icone='🦁' nome='Leão' peso={190} emExtincao={true}/>
-      <Animal icone='🦩' nome='Flamingo' peso={12} emExtincao={true}/>
-      <Animal icone='🦒' nome='Girafa' peso={1200} emExtincao={true}/>
+        fim={new Date("2025-10-11T12:00-03:00")}
+        cercado='ICE'>
+        <Animal icone='🦁' nome='Leão' peso={190} emExtincao={true} />
+        <Animal icone='🦩' nome='Flamingo' peso={12} emExtincao={true} />
+        <Animal icone='🦒' nome='Girafa' peso={1200} emExtincao={true} />
+
+      </Exibicao>
       <Exibicao inicio={new Date("2025-10-11T13:00-013:00")}
-      fim={new Date("2025-10-11T18:00-03:00")}
-      cercado='ICB'></Exibicao>
-      <Animal icone='🦜' nome='Papagaio' peso={0.4} emExtincao={false}/>
+        fim={new Date("2025-10-11T18:00-03:00")}
+        cercado='ICB'>
+        <Animal icone='🦜' nome='Papagaio' peso={0.4} emExtincao={false} />
+
+      </Exibicao>
 
     </div>
   )
--- src/Exibicao.tsx
+++ src/Exibicao.tsx
@@ -1,18 +1,21 @@
+import {type ReactNode } from "react";
 import "./Exibicao.css"
 
 type ExibicaoProps = {
     inicio: Date;
     fim: Date;
     cercado: string;
+    children?: ReactNode;
 }
 
 
-export default function Exibicao({ inicio, fim, cercado }: ExibicaoProps) {
+export default function Exibicao({ inicio, fim, cercado,children }: ExibicaoProps) {
     return (
         <div className="exibicao">
             <div className="inicio">{inicio.toLocaleString()}</div>
             <div className="fim">{fim.toLocaleString()}</div>
             <div className="cercado">{cercado}</div>
+            <div className="animais">{children}</div>
         </div>
     );
 }
