d24a7904555d1013657d9db46856c9eb57768b73
Igor Knop <igor.knop@ufjf.br>
2025-10-28 21:46:29 -0300

    transforma para vetor de elementos

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,29 @@ import './App.css'
 import Exibicao from './Exibicao'
 
 function App() {
+  const exibicao1 = [
+    <Animal icone='🦁' nome='Leão' peso={190} emExtincao={true} />,
+    <Animal icone='🦩' nome='Flamingo' peso={12} emExtincao={true} />,
+    <Animal icone='🦒' nome='Girafa' peso={1200} emExtincao={true} />
+  ];
+
+  const exibicao2 = [
+        <Animal icone='🦜' nome='Papagaio' peso={0.4} emExtincao={false} />
+
+  ];
 
   return (
     <div>
       <Exibicao inicio={new Date("2025-10-11T08:00:00-03:00")}
         fim={new Date("2025-10-11T12:00-03:00")}
         cercado='ICE'>
-        <Animal icone='🦁' nome='Leão' peso={190} emExtincao={true} />
-        <Animal icone='🦩' nome='Flamingo' peso={12} emExtincao={true} />
-        <Animal icone='🦒' nome='Girafa' peso={1200} emExtincao={true} />
+          {exibicao1}
 
       </Exibicao>
       <Exibicao inicio={new Date("2025-10-11T13:00-013:00")}
         fim={new Date("2025-10-11T18:00-03:00")}
         cercado='ICB'>
-        <Animal icone='🦜' nome='Papagaio' peso={0.4} emExtincao={false} />
-
+          {exibicao2}
       </Exibicao>
 
     </div>
